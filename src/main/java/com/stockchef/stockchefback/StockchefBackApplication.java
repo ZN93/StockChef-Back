e93b15a8e96e51a363f6dfceaf356080a9601643
bastien5967 <bastien5967@gmail.com>
2025-08-28 13:35:05 +0300

    build: :card_file_box: push ma version de Java Spring pour être sur de toujour l'avoir

---
+++ src/main/java/com/stockchef/stockchefback/StockchefBackApplication.java
@@ -0,0 +1,13 @@
+package com.stockchef.stockchefback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockchefBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockchefBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockchef/stockchefback/StockchefBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockchef.stockchefback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockchefBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
